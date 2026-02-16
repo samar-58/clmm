31774e354831a7c79d872dcde4b6b68d4e8933a1
samar sayed <sayedsamar58@gmail.com>
2026-02-16 23:23:09 +0530

    init

---
+++ programs/clmm/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("HvVeBmuPRReNPaMXXVWsz8UmtMSbUXnkGoDNN57brQcH");
+
+#[program]
+pub mod clmm {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
